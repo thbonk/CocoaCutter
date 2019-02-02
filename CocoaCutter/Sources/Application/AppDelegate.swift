1ed05f7063f6ceb01ec0c4f0e3213ffe4cbd4c19
Thomas Bonk <thomas@meandmymac.de>
2019-02-02 17:54:41 +0100

    Project creation

---
+++ CocoaCutter/Sources/Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaCutter
+//
+//  Created by Thomas Bonk on 02.02.19.
+//  Copyright © 2019 Thomas Bonk Softwareentwicklung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
