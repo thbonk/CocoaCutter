a4edc65b65eefe806a3c394ef1da5ee2896a0c16
Thomas Bonk <thomas.bonk@sap.com>
2019-02-04 13:24:45 +0100

    starting unit test implementation

--- CocoaCutterTests/CocoaCutterTests.swift
+++ CocoaCutterTests/CocoaCutterTests.swift
@@ -9,18 +9,19 @@
 import XCTest
 
 class CocoaCutterTests: XCTestCase {
+    private var otrEmail: String!
+    private var otrPassword: String!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        otrEmail = ProcessInfo.processInfo.environment["OTR_EMAIL"]
+        otrPassword = ProcessInfo.processInfo.environment["OTR_PASSWORD"]
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOtrDecoderCreation() {
+        let otrDecoder = OtrDecoder(email: otrEmail, password: otrPassword)
     }
 
     func testPerformanceExample() {
